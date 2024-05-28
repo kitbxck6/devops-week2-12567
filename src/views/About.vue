dc90e56db98050bb8b7df420867f2667496883a2
Poommipat7 <rnchayphuththrasu526@gmail.com>
2024-05-28 12:40:19 +0700

    first commit

---
+++ src/views/About.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h3>About Page</h3>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <h3>Home page</h3>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped></style>
